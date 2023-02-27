da962c8dba1f44e1aae0bd1e4e315fcac9c7b9ee
ashwinithebest <119566444+Ashwini-noob-master@users.noreply.github.com>
2023-02-27 11:09:59 +0530

    commit

---
+++ CreditCardFactory.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryDesignPattern
+{
+    public abstract class CreditCardFactory
+    {
+        protected abstract CreditCard MakeProduct();
+
+        public CreditCard CreateProduct()
+        {
+            return this.MakeProduct();
+        }
+    }
+}
+//}
+//'  public static CreditCard GetCreditCard(string cardType)
+//        {
+//    CreditCard cardDetails = null;
+
+//    if (cardType == "MoneyBack")
+//    {
+//        cardDetails = new MoneyBack();
+//    }
+//    else if (cardType == "Titanium")
+//    {
+//        cardDetails = new Titanium();
+//    }
+//    else if (cardType == "Platinum")
+//    {
+//        cardDetails = new Platinum();
+//    }
+
+//    return cardDetails;
+//}
+//'
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿
+using System;
+namespace FactoryDesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CreditCard creditCard = new PlatinumFactory().CreateProduct();
+            if (creditCard != null)
+            {
+                Console.WriteLine("Card Type : " + creditCard.GetCardType());
+                Console.WriteLine("Credit Limit : " + creditCard.GetCreditLimit());
+                Console.WriteLine("Annual Charge :" + creditCard.GetAnnualCharge());
+            }
+            else
+            {
+                Console.Write("Invalid Card Type");
+            }
+            Console.WriteLine("--------------");
+            creditCard = new MoneyBackFactory().CreateProduct();
+            if (creditCard != null)
+            {
+                Console.WriteLine("Card Type : " + creditCard.GetCardType());
+                Console.WriteLine("Credit Limit : " + creditCard.GetCreditLimit());
+                Console.WriteLine("Annual Charge :" + creditCard.GetAnnualCharge());
+            }
+            else
+            {
+                Console.Write("Invalid Card Type");
+            }
+            Console.ReadLine();
+        }
+    }
+}
+//using System;
+//namespace FactoryDesignPattern
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            CreditCard cardDetails = CreditCardFactory.GetCreditCard("Platinum");
+
+//            if (cardDetails != null)
+//            {
+//                Console.WriteLine("CardType : " + cardDetails.GetCardType());
+//                Console.WriteLine("CreditLimit : " + cardDetails.GetCreditLimit());
+//                Console.WriteLine("AnnualCharge :" + cardDetails.GetAnnualCharge());
+//            }
+//            else
+//            {
+//                Console.Write("Invalid Card Type");
+//            }
+
+//            Console.ReadLine();
+//        }
+//    }
+//}
